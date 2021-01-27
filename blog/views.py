b06ea2cdf2ac5ad94155ee079fcad4c7f9290cc1
Esat Koç <esat.koc@metu.edu.tr>
2021-01-27 15:17:44 +0300

    Comment Functionality

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import (
     Paginator,
     EmptyPage,
     PageNotAnInteger
 )
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.contrib import messages
 
@@ -42,7 +42,28 @@ def post_detail(request, year, month, day, post):
                                     publish__year=year,
                                     publish__month=month,
                                     publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+    new_comment = None
+
+    if request.method == 'POST':
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create Comment object but do not save to the database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+            comment_form = CommentForm()
+            messages.success(request, 'You commented successfully')
+    else:
+        comment_form = CommentForm()
+
+
+    return render(request, 'blog/post/detail.html', 
+                    {'post': post, 'comments': comments, 'new_comment': new_comment, 'comment_form': comment_form})
 
 
 def post_share(request, post_id):
@@ -68,3 +89,4 @@ def post_share(request, post_id):
         form = EmailPostForm()
     
     return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
